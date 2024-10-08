471a300e8fba0c17c488b2477b875cc63a9a3843
Brandon <bran2miz@gmail.com>
2024-10-08 14:46:30 -0700

    fixed styling issue with centering

--- screens/MyList.js
+++ screens/MyList.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { removeItemFromList } from '../menu/menuSlice';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import { View, Image, Text, Button, StyleSheet, ScrollView } from 'react-native';
 
 export default function MyListScreen({ restaurants }) {
     const menuItems = useSelector(state => state.menu.menuItems);
@@ -12,6 +12,7 @@ export default function MyListScreen({ restaurants }) {
     };
     console.log(restaurants);
     return (
+        <ScrollView >
         <View style={styles.container}>
             {menuItems.length === 0 ? (
                 <Text>Your Cart is Empty</Text>
@@ -25,14 +26,17 @@ export default function MyListScreen({ restaurants }) {
                     console.log(restaurant);
 
                     return (
+                       
                         <View key={idx} style={styles.card}>
                           <Text>
                             <Text style={styles.restaurantName}>{restaurant ? restaurant.name : "Unknown Restaurant"}</Text>
                             <Text> - {item.itemName}</Text>
                           </Text>
                           <Text style={styles.itemDescription}>{item.itemDescription}</Text>
+                          <Image source={require('../assets/images/FullLogo_Transparent.png')} style={styles.logo} />
                           <Button title="Remove" onPress={() => handleRemoveItemFromList(item.id)} />
                         </View>
+                        
                       );
                       
                 }
@@ -40,6 +44,7 @@ export default function MyListScreen({ restaurants }) {
                 ))
             }
         </View>
+        </ScrollView>
     );
 }
 
@@ -69,5 +74,13 @@ const styles = StyleSheet.create({
     itemDescription: {
         fontSize: 14,
         color: '#666',
+        marginTop: 10
     },
+    logo: {
+        width: 150, 
+        height: 150, 
+        marginBottom: 20,
+        resizeMode: 'contain',
+        alignSelf: 'center',  // Center the image horizontally
+      },
 });
