a02bf9b7043002b3ce0a44048b00e3050cf2f170
Brandon <bran2miz@gmail.com>
2024-10-22 08:37:13 -0700

    added rating feature. Completed the base of peckish-goose app

--- screens/ReviewModal.js
+++ screens/ReviewModal.js
@@ -1,36 +1,50 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Modal, View, Image, Text, TextInput, Button, StyleSheet } from 'react-native';
+import { Picker } from '@react-native-picker/picker';
 import { useDispatch } from 'react-redux';
 import { setReviewForItem } from '../menu/menuSlice';
 
 export default function ReviewModal({ modalVisible, closeModal, inputValue, setInputValue, itemId }) {
     const dispatch = useDispatch();
+    const [selectedRating, setSelectedRating] = useState(0);
 
     const handleSubmitReview = () => {
-        dispatch(setReviewForItem({id: itemId, review: inputValue,            
+        dispatch(setReviewForItem({
+            id: itemId, review: inputValue, rating: selectedRating
         }));
         closeModal();
     };
 
     return (
         <Modal
-        visible={modalVisible}
-        transparent={true}
-        animationType="slide"
-        onRequestClose={closeModal}
-    >
-        <View style={styles.modalContainer}>
-            <View style={styles.modalContent}>
-                <TextInput
-                    style={styles.textInput}
-                    placeholder="Enter your comment"
-                    value={inputValue}
-                    onChangeText={setInputValue}
-                />
-                <Button title="Submit" onPress={handleSubmitReview} />
+            visible={modalVisible}
+            transparent={true}
+            animationType="slide"
+            onRequestClose={closeModal}
+        >
+            <View style={styles.modalContainer}>
+                <View style={styles.modalContent}>
+                    <TextInput
+                        style={styles.textInput}
+                        placeholder="Enter your comment"
+                        value={inputValue}
+                        onChangeText={setInputValue}
+                    />
+                    <Picker
+                        selectedValue={selectedRating}
+                        onValueChange={(itemValue) => setSelectedRating(itemValue)}
+                    >
+                        <Picker.Item label="Select rating" value={0} />
+                        <Picker.Item label="⭐" value={1} />
+                        <Picker.Item label="⭐⭐" value={2} />
+                        <Picker.Item label="⭐⭐⭐" value={3} />
+                        <Picker.Item label="⭐⭐⭐⭐" value={4} />
+                        <Picker.Item label="⭐⭐⭐⭐⭐" value={5} />
+                    </Picker>
+                    <Button title="Submit" onPress={handleSubmitReview} />
+                </View>
             </View>
-        </View>
-    </Modal>
+        </Modal>
     );
 }
 
