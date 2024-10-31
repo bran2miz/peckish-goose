cad3c56d77ad1a6f799e04469db1e98f6a7318e8
Brandon <bran2miz@gmail.com>
2024-10-31 08:44:18 -0700

    created functions for saving data to local storage

--- utils/storage.js
+++ utils/storage.js
@@ -16,3 +16,23 @@ const retrieveData = async (key, value) => {
         console.error('There was an error while trying to retreive data', e);
     }
 }
+
+const saveList = async (menuItem) => {
+    const savedItem = await retrieveData('list') || [];
+    savedItem.push(menuItem);
+    await storeData('list', savedItem);
+}
+
+const getList = async () => {
+    return await retrieveData('list');
+}
+
+const clearData = async (key) => {
+    try {
+        await AsyncStorage.removeItem(key);
+    } catch (e) {
+        console.error('Error clearing data', e);
+    }
+}
+
+export {saveList, getList, clearData};
